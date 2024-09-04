2e9003e108575ff7fb12f2cc4f38ae861aea262e
SwarajPawarV2Solutions <swaraj.pawar@v2solutions.com>
2024-09-04 15:52:11 +0530

    Added 2nd project using vite in which i have use useState

---
+++ Reactjs_vite/02counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+
+import './App.css'
+
+function App() {
+
+   let [counter, setCounter] = useState(15)
+
+   //let counter = 15;
+
+
+
+   const addValue = () => {
+     //counter = counter + 1;
+     if(counter == 20){
+     setCounter(counter)
+     }else{
+      setCounter(counter + 1)
+     }
+   }
+
+   const removeValue= () =>{
+    if(counter == 0){
+    setCounter (counter - 0)
+    }else {
+      setCounter (counter - 1)
+    }
+   }
+    
+  return (
+    <>
+      <h1>Welcome</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value {counter}</button>
+      <br/>
+      <button onClick={removeValue}>Decrease Value {counter}</button>
+      <br/>
+      <p></p>
+    </>
+  )
+}
+
+export default App
